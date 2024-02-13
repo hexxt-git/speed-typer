0cb29c532cc89d542eee53d2b85344942887191a
9EED <88392191+9EED@users.noreply.github.com>
2024-02-13 18:51:52 +0100

    random words

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ class sentence{
     update_container(){
         let div = document.getElementById('text-container')
         div.innerHTML = `<span class="active">`
-        for(let i = 0 ; i < this.writing_index ; i++){
+        for(let i = 0 ; i < this.writing_index ; i++){ // stpid bad
             if(!this.error_indexes.includes(i)) div.innerHTML += this.target_text[i]
             else div.innerHTML += `<span class="error">${this.target_text[i]}</span>`
         }
@@ -30,8 +30,12 @@ class sentence{
     }
 }
 
+function word_distribution(){
+    let variation = 10
+    return Math.floor(Math.exp(-(Math.random()**2)*variation)*1000)
+}
 function random_sentence(){ // temp
-    return "the quick brown fox jumps over the lazy dog".slice(0, Math.floor(Math.random()*20+5))
+    return new Array(10).fill(0).map(e=>word_set[word_distribution()]).join(' ')
 }
 
 let current_sentence = new sentence(random_sentence());
@@ -39,12 +43,13 @@ let current_sentence = new sentence(random_sentence());
 document.addEventListener('keydown', (event)=>{
     let response = current_sentence.key_press(event.key)
     if(response === 1){
+        let text = current_sentence.target_text
         let time_ms = Date.now() - current_sentence.start_time
         let time_m = time_ms / 1000 / 60
-        let n_words = current_sentence.target_text.length / 5
+        let n_words = text.length / 5
         let wpm = n_words / time_m
-        let acc = (current_sentence.target_text.length - current_sentence.error_indexes.length) / current_sentence.target_text.length * 100
-        console.table({time_ms, time_m, n_words, wpm, acc})
+        let acc = (text.length - current_sentence.error_indexes.length) / text.length * 100
+        console.table({text, time_ms, time_m, n_words, wpm, acc})
         document.getElementById('wpm-val').innerText = Math.floor(wpm*10)/10+'wpm'
         document.getElementById('acc-val').innerText = Math.floor(acc)+'%'
         document.getElementById('time-val').innerText = Math.floor(time_ms/10)/100+'s'
